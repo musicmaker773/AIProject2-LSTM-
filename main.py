a1cd4e9dc4ede5942b37148710b04aabebdbd6fd
musicmaker773 <50848517+musicmaker773@users.noreply.github.com>
2020-12-28 00:59:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -110,29 +110,7 @@ for b in range (0,10):
     writer.writerows(lines)
 
     
-"""
-inputs = new_data[len(new_data) - len(valid) - 60:].values
-inputs = inputs.reshape(-1,1)
-inputs = scaler.transform(inputs)
 
-
-X_test = []
-for i in range(60,inputs.shape[0]):
-    X_test.append(inputs[i-60:i,0])
-X_test = np.array(X_test)
-
-X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
-
-
-closing_price = model.predict(X_test)
-closing_price = scaler.inverse_transform(closing_price)
-print(closing_price)
-
-
-train = new_data[:96]
-valid = new_data[96:]
-valid['Predictions'] = copy
-"""
 
 # print(valid[['Orange','Predictions']])
 print(train[county])
